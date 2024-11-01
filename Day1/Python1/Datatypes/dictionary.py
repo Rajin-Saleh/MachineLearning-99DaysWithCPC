e68c58f62f69f0ce60fbcd2c8dbe850ecde620d3
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 23:36:04 +0600

    Dictionaries in Python

---
+++ Day1/Python1/Datatypes/dictionary.py
@@ -0,0 +1,16 @@
+dict1 = {
+    "name": "Charlie",
+    "age": 21,
+    "Address": "America",
+    "phone": "Samsung",
+    "laptop": "Asus",
+}
+print(dict1)
+print(dict1["age"])
+print(len(dict1))
+get1 = dict1.get("laptop")
+get2 = dict1.keys()
+get3 = dict1.values()
+print(get1)
+print(get2)
+print(get3)
