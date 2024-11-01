d5b274859987dc18e03f8ae66445c8c93d5259ce
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 22:24:02 +0600

    Numbers in Python

---
+++ Day1/Python1/Datatypes/numbers.py
@@ -0,0 +1,19 @@
+# Int
+# float
+# complex
+
+n1 = 234
+n2 = 231.213
+n3 = 3245j
+
+print(type(n1))
+print(type(n2))
+print(type(n3))
+
+a = float(n1)
+b = int(n2)
+c = complex(n1)
+
+print(a)
+print(b)
+print(c)
