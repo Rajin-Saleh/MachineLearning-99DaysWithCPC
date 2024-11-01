a9bd8bf0e55115210f336bc530dc92b67b4e7aa6
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 22:46:00 +0600

    Strings in Python

---
+++ Day1/Python1/Datatypes/strings.py
@@ -0,0 +1,15 @@
+learn = "Learning String in Python"
+
+print(learn)
+
+multiline_string = """
+This is a multiline string.
+That can be used both for string and comment.
+Lets see what we can try next.
+"""
+print(multiline_string)
+
+print(type(learn))  # type of data type
+print(len(multiline_string))  # length of string
+print("learning" in learn)  # check if a word if available
+print("saw" not in multiline_string)  # check if not
