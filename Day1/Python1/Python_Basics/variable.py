7988a76626ec4b215d6321e4fffd0c71852d07c8
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 14:06:38 +0600

    variables in Python

---
+++ Day1/Python1/Python_Basics/variable.py
@@ -0,0 +1,36 @@
+#  Variables in python
+
+
+hello = "Hello Python World"
+print(hello)
+
+a = 12
+b = 76
+c = 534
+d = int(c - (a * b) + c)
+print(int(d))
+
+
+x = 3453.37684
+
+print("The type of x is", type(x))
+
+# collection of different data types or items
+
+list1 = [2, 3], [556, 67], ["Human", "Earth"]
+print(list1)
+print(type(list1))
+
+
+# same as list but can not be changed (immutable)
+
+tuple1 = (("parrots", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+print(type(tuple))
+# Collection of key value pairs = Dictionary
+
+dict1 = {"name": "Roman", "age": 20, "canVote": True}
+print(dict1)
+print(type(dict1))
+
+# 99DaysWithCPC - Machine Learning
