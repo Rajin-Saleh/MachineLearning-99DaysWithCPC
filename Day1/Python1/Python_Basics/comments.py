f3ede604703d1ca17f9b226647cd6f5c87f7adb8
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 14:32:14 +0600

    Comments in Python

---
+++ Day1/Python1/Python_Basics/comments.py
@@ -0,0 +1,13 @@
+# 99DaysWithCPC - Machine Learning
+
+
+# Commenting in Python
+
+# This is single line comment in python
+
+"""
+This is a multiline comment
+This can be done with "" too
+but for both "" or '' 3 pair is needed to
+make multiline comment
+"""
