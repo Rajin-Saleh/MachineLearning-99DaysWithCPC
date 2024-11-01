20107b7fa9bed43f8f3ed3e249eb6d4db9da49b7
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 23:18:33 +0600

    List

---
+++ Day1/Python1/Datatypes/List_Tuple.py
@@ -0,0 +1,17 @@
+# list
+
+a = ["Rohan", "Jonah", "Ummeh", "Rahul", "Khalid"]
+print(a)
+print(len(a))
+
+mixed = [True, 345, None, "abfD", "Gender"]
+print(mixed)
+
+# using list() constructor to make a new list
+
+const = list(("Car", "bike", 345, True))  # double round brackets
+print(const)
+
+# Range in index of list
+
+print(const[0:3])
