ea3bfb50c45d8c536f3e2c480706fb2a09889375
Rajin-Saleh <rajinp752@gmail.com>
2024-11-01 21:57:09 +0600

    few methods in Python

---
+++ Day1/Python1/Python_Basics/methods.py
@@ -0,0 +1,10 @@
+# 99DaysWithCPC - Machine Learning
+
+name = input("What is your name? ")
+
+print("Your name in title is", name.title())
+print("Your name in lower is", name.lower())
+print("Your name in upper is", name.upper())
+print("Your name in capitalized is", name.capitalize())
+print("Your name in casefold is", name.casefold())
+print("Your name in isalnum is", name.isalnum())
