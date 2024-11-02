60731a8fd4e8cb67782c76a04db4ca574d12bd89
Rajin-Saleh <rajinp752@gmail.com>
2024-11-02 23:26:11 +0600

    Function 1

---
+++ Day2/Python2/Functions/functionIntro.py
@@ -0,0 +1,15 @@
+def func():
+    print("Hello")
+    test = 1 + 2
+    print(test)
+
+
+def calc():
+    num1 = int(input("Enter the number 1: "))
+    num2 = int(input("Enter the number 2: "))
+    sum = num1 + num2
+    print(f"Addition of two number is {sum}")
+
+
+func()
+calc()
