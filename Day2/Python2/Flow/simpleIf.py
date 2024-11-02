b0fe4c914236c89dd52fa444ceef3db7ba8c4925
Rajin-Saleh <rajinp752@gmail.com>
2024-11-02 10:36:47 +0600

    Simple If in Python

---
+++ Day2/Python2/Flow/simpleIf.py
@@ -0,0 +1,21 @@
+x = -9
+if x > 10:
+    print("The if statement is true!")
+elif x < -1:
+    print("Statement was true")
+else:
+    print("Statement is false!")
+
+
+# simple program
+
+money_available = 46
+
+if money_available > 80:
+    print("Eat something Fancy!")
+elif money_available > 45:
+    print("Eat something nice")
+elif money_available > 15:
+    print("Eat something okay")
+else:
+    print("Eat something cheap.")
