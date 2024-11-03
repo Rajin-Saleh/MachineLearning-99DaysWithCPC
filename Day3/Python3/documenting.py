af3a0998365ceb5e5a8245ea429f3963a090cb33
Rajin-Saleh <rajinp752@gmail.com>
2024-11-03 20:42:54 +0600

    Documanting in Python

---
+++ Day3/Python3/documenting.py
@@ -0,0 +1,15 @@
+# 99DaysWithCPC - Machine Learning
+
+
+def test(a: int, b: int):
+    """A simple function that print 2 parameter"""
+
+    print(a)
+    print(b)
+    return a + b
+
+
+test(2, 3)
+
+print(test.__doc__)
+# help(test)
