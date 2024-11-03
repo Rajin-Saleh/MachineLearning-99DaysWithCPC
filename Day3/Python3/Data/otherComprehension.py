469a87c8ec96433a44815114de856428559a18a2
Rajin-Saleh <rajinp752@gmail.com>
2024-11-03 23:08:39 +0600

    Other Comprehensions with advance loop

---
+++ Day3/Python3/Data/otherComprehension.py
@@ -0,0 +1,21 @@
+# 99DaysWithCPC - Machine Learning
+
+# for loop list comprehension is the most common one
+
+set_comp = {num for num in range(100)}
+dict_comp = {num: num**2 for num in range(100)}
+tuple_comp = tuple(num for num in range(100))
+
+
+print(set_comp)
+print("\n")
+print(dict_comp)
+print("\n")
+print(tuple_comp)
+
+# exercise
+# create a dictionary with the keys 'A', 'B', 'C', 'D' and 'E'
+# each key should have a list as value with the values [1,2,3,4,5]
+
+exercise_comp = {letter: [1, 2, 3, 4, 5] for letter in "ABCDE"}
+print(exercise_comp)
