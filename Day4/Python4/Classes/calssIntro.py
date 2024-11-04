7fd35b9faef2a4908ec4add7cb981b0d59b676e9
Rajin-Saleh <rajinp752@gmail.com>
2024-11-04 10:50:05 +0600

    Begining of Class - OOP

---
+++ Day4/Python4/Classes/calssIntro.py
@@ -0,0 +1,39 @@
+# 99DaysWithCPC - Machine Learning
+
+# OOP - Object Oriented Programming
+
+# object is a container for variable and functions
+
+
+"""
+In a game
+
+if monster is a object
+health, energy, stamina, damage are the variable
+attack, movement, animations are the functions
+
+variables in an objects are called attributes
+functions in an object are called methods
+(the name is the only difference)
+
+
+It is possible to have multiple objects
+
+"""
+
+"""
+what are class?
+
+a class is the blueprint for an object
+
+a class also accept arguments to customize th object
+can also inherit from another class
+resulting object will have attributes and methods from both class
+
+why use class & object?
+1. they organize complex code
+2. they help to create reuseable code
+3. they are used in everywhere
+4. some modules requires t create classes
+5. they make it easier to work with scope
+"""
