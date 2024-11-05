90bf2d269cf8796101f8091796936704f36cc701
Rajin-Saleh <rajinp752@gmail.com>
2024-11-05 22:46:57 +0600

    Common Modules

---
+++ Day5/Python5/Modules/Standard.py
@@ -0,0 +1,21 @@
+# 99DaysWithCPC - Machine Learning
+
+# Modules - extra part we can attach in program
+
+# 1. we can import from python library
+# 2. Import additional modules made by other
+# (need to install in pc)
+
+import random, string
+from math import floor as sudo
+from datetime import datetime as dt
+
+random_number = random.randint(0, 10)
+print(random_number)
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(random.choice(test_list))
+print(string.ascii_uppercase)
+
+
+print(sudo(4.9))
+print(dt.now())
