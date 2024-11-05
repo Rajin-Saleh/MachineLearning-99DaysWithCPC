a5f9556d9a52207ebc6f91d06bfe257c46b2a687
Rajin-Saleh <rajinp752@gmail.com>
2024-11-05 23:35:11 +0600

    decorators arguments

---
+++ Day5/Python5/Extra_Parts/decorator/arguments.py
@@ -0,0 +1,20 @@
+# 99DaysWithCPC - Machine Learning
+
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("decoration begins")
+        func(*args, **kwargs)
+        print("decoration ends")
+
+    return wrapper
+
+
+@decorator
+def func(func_parameter):
+    print(func_parameter)
+
+
+# func = decorator(func)
+
+func("something")
