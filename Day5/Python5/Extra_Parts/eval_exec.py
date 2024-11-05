94683c9c370e383ddaed8540e1b0e7e9589fa289
Rajin-Saleh <rajinp752@gmail.com>
2024-11-05 23:28:14 +0600

    Eval

---
+++ Day5/Python5/Extra_Parts/eval_exec.py
@@ -0,0 +1,13 @@
+# 99DaysWithCPC - Machine Learning
+
+# print(eval('"test".upper()'))
+# exec('a = 10')
+
+my_string = "test"
+# print(my_string.upper())
+# print(my_string.title())
+# print(my_string.lower())
+# print(my_string.casefold())
+
+for operation in ["upper", "title", "lower", "casefold"]:
+    print(eval(f"my_string.{operation}()"))
