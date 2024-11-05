22e59d67cad4df59a69674747feb6e8df72730f4
Rajin-Saleh <rajinp752@gmail.com>
2024-11-05 23:24:19 +0600

    Custom Modules

---
+++ Day5/Python5/Modules/custom.py
@@ -0,0 +1,15 @@
+# 99DaysWithCPC - Machine Learning
+
+import mine
+from mine import sum_calculator
+
+# print(mine.test_var)
+# mine.test_function(123)
+
+# test = mine.Test()
+# test.do_something()
+
+# print(sum_calculator(1,2,3,4,5))
+
+if __name__ == "__main__":
+    print("the main file")
